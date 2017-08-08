8f90cd52d94cf7d491affba9d8aeb738b422341c
Roberto Rivera Guevara <A07025304@itesm.mx>
2017-08-07 21:32:47 -0500

    Update

---
+++ Triangulo/Triangulo.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "D:\Riverto\Documents\Visual Studio 2017\Projects\Project1\Circulo\Figura.h"
+class Triangulo :
+	public Figura
+{
+public:
+	Triangulo();
+protected:
+	double base, altura, hipotenusa;
+	int sen, csen, tan;
+};
+
