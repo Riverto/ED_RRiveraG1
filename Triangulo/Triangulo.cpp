65a0d6e2703cc749e7918bdd0e8e15714c1972f4
Roberto Rivera Guevara <A07025304@itesm.mx>
2017-08-07 21:59:35 -0500

    update

--- Triangulo/Triangulo.cpp
+++ Triangulo/Triangulo.cpp
@@ -2,11 +2,118 @@
 
 
 
-Triangulo::Triangulo()
+Triangulo::Triangulo(double base, double altura, double hipotenusa, int sen, int csen, int tan, double area, double perimetro)
 {
+	this->base = base;
+	this->altura = altura;
+	this->hipotenusa = hipotenusa;
+	this->sen = sen;
+	this->csen = csen;
+	this->tan = tan;
+	this->area = area;
+	this->perimetro = perimetro;
 }
 
-
-Triangulo::~Triangulo()
+Triangulo::Triangulo(double base, double altura, double hipotenusa, int sen, int csen, int tan)
 {
+	this->base = base;
+	this->altura = altura;
+	this->hipotenusa = hipotenusa;
+	this->sen = sen;
+	this->csen = csen;
+	this->tan = tan;
+}
+
+Triangulo::Triangulo(double base, double altura, double hipotenusa, int sen, int csen, int tan, Figura figura)
+{
+	this->base = base;
+	this->altura = altura;
+	this->hipotenusa = hipotenusa;
+	this->sen = sen;
+	this->csen = csen;
+	this->tan = tan;
+	this->area = figura.getArea();
+	this->perimetro = figura.getPerimetro();
+}
+
+Triangulo::Triangulo(double area, double perimetro)
+{
+	this->base = 0.0;
+	this->altura = 0.0;
+	this->hipotenusa = 0.0;
+	this->sen = 0;
+	this->csen = 0;
+	this->tan = 0;
+	this->area = area;
+	this->perimetro = perimetro;
+}
+
+Triangulo::Triangulo()
+{
+	this->base = 0.0;
+	this->altura = 0.0;
+	this->hipotenusa = 0.0;
+	this->sen = 0;
+	this->csen = 0;
+	this->tan = 0;
+}
+
+double Triangulo::getBase()
+{
+	return this->base;
+}
+
+double Triangulo::getAltura()
+{
+	return this->altura;
+}
+
+double Triangulo::getHipo()
+{
+	return this->hipotenusa;
+}
+
+double Triangulo::getSen()
+{
+	return this->sen;
+}
+
+double Triangulo::getCsen()
+{
+	return this->csen;
+}
+
+double Triangulo::getTan()
+{
+	return this->tan;
+}
+
+void Triangulo::setBase(double base)
+{
+	this->base = base;
+}
+
+void Triangulo::setAltura(double altura)
+{
+	this->altura = altura;
+}
+
+void Triangulo::setHipo(double hipo)
+{
+	this->hipotenusa = hipo;
+}
+
+void Triangulo::setSen(int sen)
+{
+	this->sen = sen;
+}
+
+void Triangulo::setCsen(int csen)
+{
+	this->csen = csen;
+}
+
+void Triangulo::setTan(int tan)
+{
+	this->tan = tan;
 }
--- Triangulo/Triangulo.h
+++ Triangulo/Triangulo.h
@@ -1,12 +1,27 @@
 #pragma once
-#include "D:\Riverto\Documents\Visual Studio 2017\Projects\Project1\Circulo\Figura.h"
+#include "Figura.h"
 class Triangulo :
 	public Figura
 {
 public:
+	Triangulo(double base, double altura, double hipotenusa, int sen, int csen, int tan, double area, double perimetro);
+	Triangulo(double base, double altura, double hipotenusa, int sen, int csen, int tan);
+	Triangulo(double base, double altura, double hipotenusa, int sen, int csen, int tan, Figura figura);
+	Triangulo(double area, double perimetro);
 	Triangulo();
+	double getBase();
+	double getAltura();
+	double getHipo();
+	double getSen();
+	double getCsen();
+	double getTan();
+	void setBase(double base);
+	void setAltura(double altura);
+	void setHipo(double hipo);
+	void setSen(int sen);
+	void setCsen(int csen);
+	void setTan(int tan);
 protected:
 	double base, altura, hipotenusa;
 	int sen, csen, tan;
 };
-
